d84f731b2447698910cc4076a6f0a51b2839269f
miha11-hash <d_mihaela@msn.com>
2024-10-26 15:42:16 +0300

    Header

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import "./globals.css";
 import { ToastProvider } from '@/context/toastContext';
 import { AuthProvider } from '@/context/authContext';
+import Header from './(components)/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
        <AuthProvider>
        <ToastProvider>
         <body className={inter.className}>
+        <Header></Header>
           <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
             {children}
           </main>
